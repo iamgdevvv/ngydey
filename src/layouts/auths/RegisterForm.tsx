44a0f7760d6538165074c1d13473073950308510
iamgdev <gn.mailwork@gmail.com>
2024-08-05 12:13:01 +0700

    fix back register form

--- src/layouts/auths/RegisterForm.tsx
+++ src/layouts/auths/RegisterForm.tsx
@@ -66,7 +66,7 @@ export default function RegisterForm({ className = '' }: Prop) {
 				label='Email'
 				size='md'
 				mb='md'
-				{...form.getInputProps('emailz')}
+				{...form.getInputProps('email')}
 			/>
 			<PasswordInput
 				label='Password'
